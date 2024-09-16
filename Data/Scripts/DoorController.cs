552d861755f44361096b9cf7ecec3658001a73af
Robosapien6219 <91446426+Robosapien6219@users.noreply.github.com>
2024-09-16 21:08:50 +1000

    Doors

---
+++ Data/Scripts/DoorController.cs
@@ -0,0 +1,39 @@
+using System;
+using BellyRub;
+
+namespace CSharpAssembly
+{
+	public class DoorController : ScriptController
+	{
+		public Entity m_DoorEntity                               = null;
+		private SpriteComponent m_SpriteComponent                 = null;
+		private SpriteAnimatorComponent m_SpriteAnimatorComponent = null;
+
+		void Start()
+		{
+			if(m_DoorEntity == null)
+			{
+				Debug.LogError("We Have no Door ya dick head!");
+				return;
+			}
+			
+			m_SpriteComponent         = m_DoorEntity.GetComponent<SpriteComponent>();
+			m_SpriteAnimatorComponent = m_DoorEntity.GetComponent<SpriteAnimatorComponent>();
+		}
+
+		void TriggerEnter(Entity other, Vector3 normal)
+		{
+			//play openning animation
+			m_SpriteAnimatorComponent.SetCurrentAnimation(1);
+			//diable collider
+			
+		}
+		
+		void TriggerExit(Entity other, Vector3 normal)
+		{
+			//play closing animation, (open in reverse)
+			m_SpriteAnimatorComponent.SetCurrentAnimation(0);
+			//enable collider
+		}
+	}
+}
